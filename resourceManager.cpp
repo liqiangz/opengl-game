926031eec0e924b13afab9152dd08abcde411244
李强 <15255829911@163.com>
2017-02-11 20:38:26 +0800

    init

--- resourceManager.cpp
+++ resourceManager.cpp
@@ -80,7 +80,6 @@ Shader ResourceManager::loadShaderFromFile(const GLchar *vShaderFile, const GLch
     const GLchar *vShaderCode = vertexCode.c_str();
     const GLchar *fShaderCode = fragmentCode.c_str();
     const GLchar *gShaderCode = geometryCode.c_str();
-    // 2. Now create shader object from source code
     Shader shader;
     shader.Compile(vShaderCode, fShaderCode, gShaderFile != nullptr ? gShaderCode : nullptr);
     return shader;
